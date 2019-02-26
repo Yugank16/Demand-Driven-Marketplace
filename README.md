1c1fe8616316c4fec237e4ed1455040e12a2b3ee
yuagnk Sharma <yugank.sharma@joshtechnologygroup.com>
2019-02-26 17:01:05 +0530

    Initial Setup

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # Demand Driven Marketplace
 
+
+Demand driven marketplace for household items(old/new/2nd hand everything). Buyer will post their requirement with budget, where seller(any platform user) can bid for that requirement.
+
+Requirements
+
+Users: Any user can sign up as platform as buyer, seller or both. A user can invite more users at platform(invite via csv file also supported) but max. 25 users a day only. 
+There can be multiple groups in our system. Any user can create a group and the owner can add one or more admins to the group.
+
+Item:
+A user can post their requirement as form of Item.
+
+Item Bid: Any user can bid for that item requirement by providing product details(min. 6 photo and other details). Item requester can not view a bid price until a bid disclose.
+
+Public Bid vs Group specific bid: An item requester can open the bid for all or specific to groups.
+
+Eligible bidder: Before a bid disclose, item requester can reject a seller bid after viewing  product specification/details. Platform will refund bid amount in next 2 hours. 
+
+Bid disclose: A eligible bidder/seller with minimum sell price will be selected as bid winner.
+
+# Django Setup (for ubuntu)
+
+0- sudo apt-get update
+
+1- sudo apt-get install python-pip
+
+2- pip install Django==1.11.20
+
+3- You can use any editor to work. For Example, lets install  Visual Studio.
+
+# Installing Visual Studio (Optional)
+
+1 Download Visual Studio Code
+  https://code.visualstudio.com/download
+2 Install and Open Visual Code
+3 In the extensions tab, download python extension.
+
+
+
+#Start Project
+
+1 Clone project to your directory.
+2 Make a local_setting.py file where settings file is present and add the following code:
+
+	import settings
+
+	DATABASES = {
+    		'default': {
+			'ENGINE': '<Write your database engine>',
+			'NAME': ' '<database name>',
+			'USER': '<db user>',
+			'PASSWORD': '<db password>',
+			'HOST': '<your host>',
+			'PORT': '<your port>',
+    			}
+		}
+
+3 Add following code in the last of your settings.py file.
+
+	try:
+   		from local_settings import *
+	except ImportError:
+    		raise ImportError("A local_settings.py file is required to run this project")
+
+# Run Project
+
+1 Run $ python manage.py migrate to run migrations
+2 Run server, $ python manage.py runserver
+3 Open http://127.0.0.1:8000/ in your browser.
+
+
---
+++ demand_driven_marketplace_api/demand_driven_marketplace_api/settings.py
@@ -0,0 +1,98 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+SECRET_KEY = '6p@m9g4l&p)93rx5b=+y=bu3e%4#490c^$jrx*7251+kkux+(p'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'demand_driven_marketplace_api.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'demand_driven_marketplace_api.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<DATABASE_NAME>',
+        'USER': '<DATABASE_USER>',
+        'PASSWORD': '<DATABASE_PASSWORD>',
+        'HOST': '<DATABASE_HOST>',
+        'PORT': '<DATABASE_PORT>',
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+STATIC_URL = '/static/'
+
+
+try:
+   from local_settings import *
+except ImportError:
+    raise ImportError("A local_settings.py file is required to run this project")
